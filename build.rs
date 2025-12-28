6a8e3ff36750855162c797f2a503e99f5b3be14a
D1360-64RC14 <gdiego416@gmail.com>
2025-12-28 15:53:16 -0300

    WIP!: worked on project files

---
+++ build.rs
@@ -0,0 +1,107 @@
+use std::{
+    path::Path,
+    process::{Command, Output},
+};
+
+use walkdir::WalkDir;
+
+const PATH_DATA_UI: &str = "data/ui";
+const PATH_DATA_GRESOURCE: &str = "data";
+
+fn main() {
+    build_ui();
+    build_gresource();
+}
+
+fn build_ui() {
+    let ui_path = Path::new(PATH_DATA_UI);
+    println!("cargo:rerun-if-changed={}", PATH_DATA_UI);
+
+    let blueprint_paths = WalkDir::new(ui_path) //
+        .into_iter()
+        .filter_map(|item| match item {
+            Ok(item) if item.path().is_file() && item.path().ends_with(".blp") => {
+                match item.path().to_str() {
+                    Some(path_str) => Some(path_str.to_owned()),
+                    None => None,
+                }
+            }
+            _ => None,
+        })
+        .collect::<Vec<String>>();
+
+    if blueprint_paths.is_empty() {
+        eprintln!("no blueprint files found");
+        return;
+    }
+
+    let blueprint_compiler_output = Command::new("blueprint-compiler") //
+        .arg("batch-compile")
+        .arg("data/ui/compiled")
+        .arg("data/ui")
+        .args(blueprint_paths)
+        .output()
+        .expect("unable to run blueprint-compiler");
+
+    if blueprint_compiler_output.status.success() {
+        evaluate_output("blueprint-compiler succeeded", blueprint_compiler_output);
+    } else {
+        evaluate_output("blueprint-compiler failed", blueprint_compiler_output);
+    };
+}
+
+fn build_gresource() {
+    let gresource_path = Path::new(PATH_DATA_GRESOURCE);
+    println!("cargo:rerun-if-changed={}", PATH_DATA_GRESOURCE);
+
+    let resource_paths = WalkDir::new(gresource_path) //
+        .into_iter()
+        .filter_map(|item| match item {
+            Ok(item) if item.path().is_file() && item.path().ends_with(".gresource.xml") => {
+                match item.path().to_str() {
+                    Some(path_str) => Some(path_str.to_owned()),
+                    None => None,
+                }
+            }
+            _ => None,
+        });
+
+    for path in resource_paths {
+        let glib_compile_resources_output = Command::new("glib-compile-resources") //
+            .arg(path)
+            .output()
+            .expect("unable to run glib-compile-resources");
+
+        if glib_compile_resources_output.status.success() {
+            evaluate_output(
+                "glib-compile-resources succeeded",
+                glib_compile_resources_output,
+            );
+        } else {
+            evaluate_output(
+                "glib-compile-resources failed",
+                glib_compile_resources_output,
+            );
+        }
+    }
+}
+
+fn evaluate_output(msg: &str, output: Output) {
+    eprintln!("{}", msg);
+
+    eprint!(
+        "{}",
+        String::from_utf8_lossy(match output.status.success() {
+            true => &output.stdout,
+            false => &output.stderr,
+        })
+        .lines()
+        .map(|line| format!("| {}", line))
+        .collect::<Vec<String>>()
+        .join("\n")
+    );
+
+    if !output.status.success() {
+        std::process::exit(output.status.code().unwrap_or(0));
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use gtk::{
+    gio::prelude::{ApplicationExt, ApplicationExtManual},
+    prelude::{GtkWindowExt, WidgetExt},
+};
+
+const APP_NAME: &str = "Alternate";
+const APP_DOMAIN: &str = "br.dev.diegogarcia.gnome";
+
+const APP_ID: &str = APP_DOMAIN + "." + APP_NAME;
+const APP_PATH: &str = "/" + APP_ID.replace(".", "/");
+
+fn main() -> gtk::glib::ExitCode {
+    let app = adw::Application::builder() //
+        .application_id(APP_ID)
+        .build();
+
+    app.connect_activate(&build_main_window);
+
+    app.run()
+}
+
+fn build_main_window(app: &adw::Application) {
+    // APP_ID.to_string()
+
+    let window: adw::ApplicationWindow =
+        gtk::Builder::from_file(format!("{}/data/ui/compiled/window.ui", APP_ID)) //
+            .object("root")
+            .unwrap();
+
+    window.set_application(Some(app));
+    window.set_title(Some(APP_NAME));
+
+    window.add_css_class("devel");
+    window.present();
+}
